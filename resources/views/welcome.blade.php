738af30c3adf46f903120fee26217c38c5a90ec3
halashaker <halachaker44@gmail.com>
2021-06-07 15:10:14 +0300

    migration hala

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,12 +8,6 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
-
-
-
-
-
         <!-- Styles -->
         <style>
             html, body {
@@ -90,14 +84,14 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs">hala</a>
+                    <a href="https://laracasts.com">shaker</a>
+                    <a href="https://laravel-news.com">is </a>
+                    <a href="https://blog.laravel.com">the</a>
+                    <a href="https://nova.laravel.com">best</a>
+                    <a href="https://forge.laravel.com">allways</a>
+                    <a href="https://vapor.laravel.com">thank</a>
+                    <a href="https://github.com/laravel/laravel">you</a>
                 </div>
             </div>
         </div>
