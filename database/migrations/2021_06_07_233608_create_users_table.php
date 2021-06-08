10fe24506d8b8166e04f09bf82110b2bab035a18
halashaker <halachaker44@gmail.com>
2021-06-08 13:38:34 +0300

    ..

--- database/migrations/2021_06_07_233608_create_users_table.php
+++ database/migrations/2021_06_07_233608_create_users_table.php
@@ -16,12 +16,16 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('fullname')->nullable();
+            $table->string('username')->nullable();
+            $table->string('birth')->nullable();
+            $table->string('date_grad')->nullable();
              $table->bigInteger('phone')->nullable();
-             $table->enum('gender',['MALE','FEMALE'])->default('MALE');
 
              $table->bigInteger('role_id')->unsigned();
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
 
+            $table->string('notes')->nullable();
+            $table->string('image')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
