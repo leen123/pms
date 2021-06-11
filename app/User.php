31135812c24fabe127900923be53d8f78e3ec968
halashaker <halachaker44@gmail.com>
2021-06-11 16:07:54 +0300

    >>

--- app/User.php
+++ app/User.php
@@ -1,10 +1,49 @@
 <?php
 
-namespace App;
+/*namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
 
+} */
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'fullname', 'email', 'password','username','birth','date_grad','phone','role_id','notes','image',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
+
