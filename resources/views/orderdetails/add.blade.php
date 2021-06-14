a8eddd95b55770395a68c182d6d695b3585b9850
leen123 <58842963+leen123@users.noreply.github.com>
2021-06-14 18:14:28 +0300

    lo

--- resources/views/orderdetails/add.blade.php
+++ resources/views/orderdetails/add.blade.php
@@ -10,10 +10,37 @@
 
 <form action="{{route('store-orderdetail')}}" method="POST">
 	@csrf
+
   <div class="form-group">
-    <label for="name">Name:</label>
-    <input type="text" class="form-control" placeholder="Enter name" id="name" name="name">
+    <label for="quantity">Quantity:</label>
+    <input type="number" class="form-control" placeholder="Enter quantity" id="quantity" number="quantity">
   </div>
+
+  <div class="form-group">
+    <label for="price">Price:</label>
+    <input type="number" class="form-control" placeholder="Enter price" id="price" number="price">
+  </div>
+
+  <div class="form-group">
+    <label for="order_id">order:</label>
+    <select class="form-control" name="order_id">
+      @foreach($order as $order)
+      <option value="{{$order->id}}">{{$order->id}}</option>
+      @endforeach
+      </select>
+   </div>
+
+   <div class="form-group">
+    <label for="product_id">product:</label>
+    <select class="form-control" name="order_id">
+      @foreach($product as $product)
+      <option value="{{$product->id}}">{{$product->id}}</option>
+      @endforeach
+      </select>
+   </div>
+
+
+
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 
