cab2a489d40cd34e6e0e864b08d9bbf034f77ef7
marymar_radwan <marymarradwan@gmail.com>
2021-06-16 17:13:24 +0300

    page

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,26 +10,59 @@ class PagesController extends Controller
 
          return view ('add-medicine');
      }
-    public function contact(){
-         return view ('contact');
+    public function contact_us(){
+         return view ('contact-us');
   }
-  public function about(){
+  public function dashboard(){
 
-         return view ('about');
+         return view ('ashboard');
   }
-  public function gallery(){
+  public function edit_inputs_setting(){
 
-         return view ('gallery');
-  }
-
-  public function pricing(){
-
-         return view ('pricing');
-  }
-  public function services(){
-
-         return view ('services');
+         return view ('edit-inputs-setting');
   }
 
+  public function edit_medicine(){
+
+         return view ('edit-medicine');
+  }
+  public function general_settings(){
+
+         return view ('general-settings');
+  }
+
+  public function index(){
+
+    return view ('index');
+}
+
+public function launcher(){
+
+    return view ('launcher');
+}
+
+public function main(){
+
+    return view ('main');
+}
+public function my_account(){
+
+    return view ('my-account');
+}
+
+public function new_bill(){
+
+    return view ('new-bill');
+}
+
+public function remove_medicine(){
+
+    return view ('remove-medicine');
+}
+
+public function search_for_medicine(){
+
+    return view ('search-for-medicine');
+}
 
 }
